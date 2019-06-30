9197930526950428095ae4aa1859862c4847538f
Gheorghe Cristian <gheorghecristian03@yahoo.com>
2019-06-30 12:40:19 +0300

    JavaScript tema FunctionsLoops

---
+++ java-script/Homework-JSFunctionsLoops/JSFunctions.js
@@ -0,0 +1,49 @@
+//Scrieti o functie de tip function expression asociata cu numele
+//helloWorldInLang care:
+
+//CONSOLE.LOG
+/*var helloWorldInLang = function(language) {
+    if(language === 'en') {
+        console.log('Hello World');
+    } else if(language === 'es') {
+        console.log('Hola Mundo');
+    } else if(language === 'de') {
+        console.log('Hallo Wereld');
+    } else {
+        console.log('Hello World');
+    }
+}
+helloWorldInLang('es');*/
+
+//RETURN
+/* var helloWorldInLang = function(language){
+    if(language === "en") return 'Hello World';
+    if(language === "es") return 'Hola Mundo';
+    if(language === "de") return 'Hallo Wereld';
+    if(language === "") return 'Hello World';
+}
+console.log(helloWorldInLang(''))*/
+
+// 2.Creati o functie care afiseaza cel mai mic numar dintr-un array
+
+
+//function minNumber(arr){
+//    var min = arr[0];
+//    for(var i = 0; i < arr.length; i++) {
+//    if(arr[i]<min){
+//        min = arr[i];
+//        console.log(min);}
+//}
+//}
+//minNumber([2, 1, 90, 44, 32, 77, 15, 0]);
+
+
+//3.Numere prime
+
+//function prime(n) {
+//      for ( i = 2; i < n; i++) {
+//       if (n % i == 0) return false;
+//    }
+//    return true;
+//}
+//console.log(prime(112));
---
+++ java-script/js_saptamana7.js
@@ -0,0 +1,48 @@
+// Creati o functie care returneaza(true/false) daca un numar este primit ca argument este prim.
+// Conditii pt a fi numar prim:
+// >1 (pozitiv), sa nu fie 2
+
+/*function returnPrimeN (n){
+    if(n <= 1 || n % 2 === 0) return false;
+    if (n == 2) return true;
+
+    for (var i=3; i <= Math.sqrt(n); i+=2){
+        if(n % i === 0) { return false;}
+    } 
+return true;
+}
+console.log(returnPrimeN(4))*/
+
+//2.Creati o functie care primeste ca argumente un sir de caracter si un numar
+//si returneaza sirul de caractere repetat de cate ori specifica numarul.
+
+/*function repeatString(str, n) {
+    var result ='';
+    for (var i = 0; i < n; i++){
+    result = str + result;
+} return result;
+}
+console.log(repeatString('ceva',1));*/
+
+//3. exercitiul cu taxele
+
+/*function calculateTax(salary, tax){
+    return 'Taxa este' + tax + '%. La ANAF se duc'+ (salary * tax)/100;
+
+}
+
+
+var venit = function(remuneratie) {
+    if(remuneratie <= 30000) {
+        return 'Taxa este 0%.'
+    }
+    if(remuneratie > 30000 && remuneratie < 50000){
+        return calculateTax(remuneratie, 50);
+    }
+}
+console.log(venit(30001));*/
+
+var cantaret = ['Dua Lipa', 'Gabriel Cotabila','Nu Am','The Weeknd','Elena Gheorghe',]
+
+
+console.log(cantaret.indexOf('Gabriel Cotabita'));
